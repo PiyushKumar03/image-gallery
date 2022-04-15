bd071d78fe818e1cf6bd5ff85ede7b26b3fa8352
Piyush Kumar <piyushkumarpiyushkumar6@gmail.com>
2022-04-16 02:36:30 +0530

    Updates

---
+++ precache-manifest.189f2527e3f2bc8abb4c4d1df998d7e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59e51e11f033eefe1f2fb3d131b28d1e",
+    "url": "/image-gallery/index.html"
+  },
+  {
+    "revision": "9461462643863634a361",
+    "url": "/image-gallery/static/css/main.096afcfd.chunk.css"
+  },
+  {
+    "revision": "5d50c1a52638fbda4191",
+    "url": "/image-gallery/static/js/2.3f381743.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/image-gallery/static/js/2.3f381743.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9461462643863634a361",
+    "url": "/image-gallery/static/js/main.3bd53917.chunk.js"
+  },
+  {
+    "revision": "d8bb13b07e2cca71210a",
+    "url": "/image-gallery/static/js/runtime-main.84ea243f.js"
+  }
+]);
